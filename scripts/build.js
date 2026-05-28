2c517e0aad53b342444bd3494b83c111fece5b20
k33n26 <37938131+k33n26@users.noreply.github.com>
2026-05-28 19:52:04 +0300

    Create build.js

---
+++ scripts/build.js
@@ -0,0 +1,56 @@
+import fs from "fs";
+
+const worker =
+  "https://dmtv.k33n26.workers.dev";
+
+
+const lines =
+  fs
+    .readFileSync(
+      "channels.txt",
+      "utf8"
+    )
+    .split("\n")
+    .filter(Boolean);
+
+
+let m3u =
+  "#EXTM3U\n";
+
+
+for (const line of lines) {
+
+  const [
+    name,
+    id,
+    logo,
+    group
+  ] =
+    line
+      .split("|")
+      .map(
+        x =>
+          x.trim()
+      );
+
+
+  m3u +=
+`#EXTINF:-1 tvg-name="${name}" tvg-logo="${logo}" group-title="${group}",${name}
+${worker}/${id}.m3u8
+
+`;
+}
+
+
+fs.mkdirSync(
+  "playlist",
+  {
+    recursive:true
+  }
+);
+
+
+fs.writeFileSync(
+  "playlist/index.m3u",
+  m3u
+);
